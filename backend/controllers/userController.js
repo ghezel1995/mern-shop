40d56f95324feb8ef07124f0f6b8b380de65c224
Mahsa <mahsa.ghezel.1995@gmail.com>
2024-11-02 09:16:12 +0330

    Registration and Encryption

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import asyncHandler from '../middleware/asyncHandler.js';
-import jwt from 'jsonwebtoken';
 import User from '../models/userModel.js';
+import generateToken from '../utils/generateToken.js';
 
 //@desc Auth user & get token
 //@route POST /api/users/login
@@ -11,16 +11,7 @@ const authUser = asyncHandler(async (req, res) => {
   const user = await User.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-      expiresIn: '30d',
-    });
-    // Set JWT as an HTTP-Only cookie
-    res.cookie('jwt', token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
-      sameSite: 'strict', // Prevent CSRF attacks
-      maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days in milliseconds
-    });
+    generateToken(res, user._id);
     res.json({
       _id: user._id,
       name: user.name,
@@ -37,7 +28,34 @@ const authUser = asyncHandler(async (req, res) => {
 //@route POST /api/users
 //@access Public
 const registerUser = asyncHandler(async (req, res) => {
-  res.send('register user');
+    const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      res.status(400);
+      throw new Error('User already exists');
+    }
+
+  const user = await User.create({
+    name,
+    email,
+    password, //encryption of the password in the user model
+  });
+
+  if (user) {
+     generateToken(res, user._id);
+    res.status(201).json({
+      //Status 201 everything is good and st is created
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+        res.status(400);
+        throw new Error('Invalid user data');
+  }
 });
 
 //@desc Logout user / clear cookie
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -31,6 +31,16 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
+//pre does allow us to do st before it's saved in the DB
+//We're going to hash the password before it's going to save in the DB
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
 const User = mongoose.model('User', userSchema);
 
 export default User;
